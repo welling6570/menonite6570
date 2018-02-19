bbc474685a1862d47009abc88d9a2d13a122eb9f
welling6570 <35569254+welling6570@users.noreply.github.com>
2018-02-19 09:04:46 -0500

    Revert "Revert "I did some work on the code.    -  <3 Alex""
    This reverts commit d0385e5447b0262c80bdb1acc1a2f4c789f9a038.

--- menonite/src/org/usfirst/frc6570/menonite/commands/usainbolt.java
+++ menonite/src/org/usfirst/frc6570/menonite/commands/usainbolt.java
@@ -12,6 +12,7 @@
 package org.usfirst.frc6570.menonite.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc6570.menonite.Robot;
+import org.usfirst.frc6570.menonite.OI;
 
 /**
  *
@@ -38,7 +39,7 @@ public class usainbolt extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
-    	Robot.driveyboi.
+    	Robot.driveyboi.joydrive();
     }
 
     // Called repeatedly when this Command is scheduled to run
